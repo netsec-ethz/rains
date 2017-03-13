cec847e80ee044e7419f01f6da897ce10037fa9b
fehlmach@student.ethz.ch <fehlmach@student.ethz.ch>
2017-03-13 17:02:42 +0100

    outlined design of inbox

--- rainsd/inbox.go
+++ rainsd/inbox.go
@@ -1,7 +1,30 @@
 package rainsd
 
+//add prio1 queue
+//add prio2 queue
+//create token cache to differentiate self issued and external queries
+
+func init() {
+	//TODO CFE initialize both queues and the token cache
+	queueWorker()
+}
+
 //Deliver handles all incoming messages. It verifies the signatures on the message, parses the query options,
 //check expiration date and if it is valid sends it on to the engine which then processes the message.
-func Deliver(message string, sender ConnInfo) {
+func Deliver(msg string, sender ConnInfo) {
+	//TODO check if self issued query
+	//TODO CFE push to correct queue
+
 	SendTo("new message", sender)
+}
+
+func queueWorker() {
+	//TODO CFE create go routines (handleMessage) which process the messages on the queue
+}
+
+func handleMessage(msg string) {
+	msg = Verify(msg)
+	//TODO CFE parse query options
+	//TODO CFE check expiration date
+	//TODO CFE forward packet
 }
