9ed574213198b65f6e4720f76b515cd66abbec19
Brian Trammell <brian@trammell.ch>
2017-03-20 13:05:53 +0100

    wip

--- zonefiles/rzfc.py
+++ zonefiles/rzfc.py
@@ -6,7 +6,13 @@
 
 # NOTE: this implements zonefile declarations in zone-context order, the
 # design document (which is normative) uses context-zone order. Change this
-# code.
+# code. NOTE NOTE: in progress.
+
+# NOTE: EXTRAKEY not yet supported. Change this code.
+
+# NOTE: Keyspace identifiers not yet supported. Change this code.
+
+# NOTE: need to support reverse assertions as in draft
 
 import re
 import cbor
@@ -14,44 +20,47 @@ from ipaddress import ip_address
 from collections import namedtuple
 
 K_SIGNATURES     = 0
-K_CAPABILITIES   = 1 
-K_TOKEN          = 2 
-K_SUBJECT_NAME   = 3 
-K_SUBJECT_ZONE   = 4 
-K_QUERY_NAME     = 5 
-K_CONTEXT        = 6 
-K_OBJECTS        = 7 
+K_CAPABILITIES   = 1
+K_TOKEN          = 2
+K_SUBJECT_NAME   = 3
+K_SUBJECT_ZONE   = 4
+K_QUERY_NAME     = 5
+K_CONTEXT        = 6
+K_OBJECTS        = 7
 K_QUERY_TYPES    = 9
 K_QUERY_OPTS     = 10
 K_SHARD_RANGE    = 11
 K_SUBJECT_ADDR   = 12 # FIXME document in draft
 K_NOTE_TYPE      = 21
 K_NOTE_DATA      = 22
-K_CONTENT        = 23 
+K_CONTENT        = 23
 
-OBJ_NAME         = 1 
-OBJ_IP6          = 2 
-OBJ_IP4          = 3 
-OBJ_REDIRECTION  = 4 
-OBJ_DELEGATION   = 5 
-OBJ_NAMESET      = 6 
-OBJ_CERT_INFO    = 7 
-OBJ_SERVICE_INFO = 8 
-OBJ_REGISTRAR    = 9 
+OBJ_NAME         = 1
+OBJ_IP6          = 2
+OBJ_IP4          = 3
+OBJ_REDIRECTION  = 4
+OBJ_DELEGATION   = 5
+OBJ_NAMESET      = 6
+OBJ_CERT_INFO    = 7
+OBJ_SERVICE_INFO = 8
+OBJ_REGISTRAR    = 9
 OBJ_REGISTRANT   = 10
 OBJ_INFRAKEY     = 11
+OBJ_EXTRAKEY     = 12 # FIXME add support for extrakey / keyspaces
 OBJ_ANYADDR      = 23 # FIXME document in draft
 
-SEC_ASSERTION     = 1 
-SEC_SHARD         = 2 
-SEC_ZONE          = 3 
-SEC_QUERY         = 4 
+SEC_ASSERTION     = 1
+SEC_SHARD         = 2
+SEC_ZONE          = 3
+SEC_QUERY         = 4
 SEC_NOTIFICATION  = 23
 
-ALGORITHMS = { "ed25519"  : 0,
-               "ed448"    : 1,
-               "ECDSA256" : 2,
-               "ECDSA384" : 3}
+ALGORITHMS = { "ed25519"  : 1,
+               "ed448"    : 2,
+               "ECDSA256" : 3,
+               "ECDSA384" : 4}
+
+KEYSPACES = { "rains"     : 0 }
 
 Token = namedtuple("Token", ["t", "v", "p"])
 
@@ -76,6 +85,7 @@ scanner = re.Scanner([
     (r":redir:\s+",     lambda s,t:(Token(t.rstrip(),         None, s.match.span()))),
     (r":cert:\s+",      lambda s,t:(Token(t.rstrip(),         None, s.match.span()))),
     (r":infra:\s+",     lambda s,t:(Token(t.rstrip(),         None, s.match.span()))),
+    (r":extra:\s+",     lambda s,t:(Token(t.rstrip(),         None, s.match.span()))),
     (r":nameset:\s+",   lambda s,t:(Token(t.rstrip(),         None, s.match.span()))),
     (r":regr:\s+",      lambda s,t:(Token(t.rstrip(),         None, s.match.span()))),
     (r":regt:\s+",      lambda s,t:(Token(t.rstrip(),         None, s.match.span()))),
@@ -89,8 +99,8 @@ scanner = re.Scanner([
     (r"([0-9a-fA-F]{1,4}:){0,7}:([0-9a-fA-F]{1,4}:){0,7}[0-9a-fA-F]{1,4}",
                         lambda s,t:(Token("VAL_IP6", ip_address(t), s.match.span()))),
     (r"\d+-\d+-\d+T\d+:\d+:\d+",
-                        lambda s,t:(Token("VAL_8601",    
-                            (datetime.strptime(t,"%Y-%m-%dT%H:%M:%S") - 
+                        lambda s,t:(Token("VAL_8601",
+                            (datetime.strptime(t,"%Y-%m-%dT%H:%M:%S") -
                              datetime(1970,0,1)).total_seconds(), s.match.span()))),
     (r"[1-9][0-9]*",
                         lambda s,t:(Token("VAL_INT",             t, s.match.span()))),
@@ -99,8 +109,8 @@ scanner = re.Scanner([
     (r":[a-zA-Z][a-zA-Z0-9_]*:\s+",
                         lambda s,t:(Token("TOK_RESERVED",     None, s.match.span()))),
     (r"\S+",            lambda s,t:(Token("VAL_STRING",          t, s.match.span()))),
-    (r"\s+",            lambda s,t:None),
-    (r"#\.*\n",         lambda s,t:None)
+    (r"\s+",            lambda s,t:None), # ignore whitespace
+    (r"#\.*\n",         lambda s,t:None)  # ignore comments
 ])
 
 
@@ -121,61 +131,71 @@ def is_object_sym(tok):
            tok.t == ":redir:" or
            tok.t == ":cert:" or
            tok.t == ":infra:" or
+           tok.t == ":extra:" or
            tok.t == ":nameset:" or
            tok.t == ":regr:" or
            tok.t == ":regt:" or
            tok.t == ":srv:")
 
 
-def consume_symbol(ts, sym):
+def _consume_symbol(ts, sym):
     if ts[0].t != sym:
         raise ValueError("expected "+str(sym)+", got "+str(ts.v), "at "+str(ts[0].p[0]))
     return ts[1:]
 
-def section(ts):
+def _message(ts):
+    content = []
+
+    while len(ts):
+        sec, ts = _section(ts)
+        content.append(sec)
+    
+    return { K_CONTENT: content }
+
+def _section(ts):
     if ts[0].t == ":Z:":
         if not is_string(ts[1]) or not is_string(ts[2]) :
             raise ValueError("missing zone and/or context in :Z: at"+str(ts[0].p[0]))
-        z, ts = zone(ts[3:], ts[1].v, ts[2].v)
-        return [ SEC_ZONE, z ]
+        z, ts = _zone(ts[3:], ts[1].v, ts[2].v)
+        return [ SEC_ZONE, z ], ts
     elif ts[0].t == ":S:":
         if not is_string(ts[1]) or not is_string(ts[2]) :
             raise ValueError("missing zone and/or context in bare :S: at"+str(ts[0].p[0]))
-        s, ts = shard(ts[3:], ts[1].v, ts[2].v, True)
+        s, ts = _shard(ts[3:], ts[1].v, ts[2].v, True)
         return [ SEC_SHARD, s], ts
     elif ts[0].t == ":A:":
         if not is_string(ts[1]) or not is_string(ts[2]) :
             raise ValueError("missing zone and/or context in bare :A: at"+str(ts[0].p[0]))
-        a, ts = assertion(ts[3:], ts[1].v, ts[2].v, True)
-        return [ SEC_ASSERTION, a ]
+        a, ts = _assertion(ts[3:], ts[1].v, ts[2].v, True)
+        return [ SEC_ASSERTION, a ], ts
     else:
         raise ValueError("expected :Z:, :S:, or :A: at "+str(ts[0].p[0]))
 
-def zone(ts, context_name, zone_name):
+def _zone(ts, context_name, zone_name):
     out = { K_SUBJECT_ZONE:    zone_name,
             K_CONTEXT:      context_name,
             K_CONTENT:      [] }
 
-    ts = consume_symbol(ts, "[")
+    ts = _consume_symbol(ts, "[")
 
     # eat content
     while ts[0].t != "]":
         if ts[0].t == ":S:":
-            s, ts = shard(ts[1:], context_name, zone_name, False)
+            s, ts = _shard(ts[1:], context_name, zone_name, False)
             out[K_CONTENT].append(s)
         elif ts[0].t == ":A:":
-            a, ts = assertion(ts[1:], context_name, zone_name, False)
+            a, ts = _assertion(ts[1:], context_name, zone_name, False)
             out[K_CONTENT].append(a)
         else:
             raise ValueError("expected :S:, :A:, or ] at "+str(ts[0].p[0]))
-    ts = consume_symbol(ts, "]")
+    ts = _consume_symbol(ts, "]")
 
     # and signatures, if present
-    out[K_SIGNATURES], ts = signatures(ts)
+    out[K_SIGNATURES], ts = _signatures(ts)
 
     return out, ts
 
-def shard(ts, context_name, zone_name, is_section):
+def _shard(ts, context_name, zone_name, is_section):
     out = { K_SUBJECT_ZONE:    zone_name,
             K_CONTEXT:      context_name,
             K_CONTENT:      [],
@@ -186,7 +206,7 @@ def shard(ts, context_name, zone_name, is_section):
         ts = ts[1:]
         if is_string(ts[0]):
             out[K_SHARD_RANGE].append(ts[0].v)
-            ts = consume_symbol(ts[1:],",")
+            ts = _consume_symbol(ts[1:],",")
         elif ts[0].t == ",":
             out[K_SHARD_RANGE].append(None)
             ts = ts[1:]
@@ -200,34 +220,34 @@ def shard(ts, context_name, zone_name, is_section):
             out[K_SHARD_RANGE].append(None)
         else:
             raise ValueError("expected shard range end or ) at "+str(ts[0].p[0]))
-        ts = consume_symbol(ts, ")")
+        ts = _consume_symbol(ts, ")")
 
-    ts = consume_symbol(ts, "[")
+    ts = _consume_symbol(ts, "[")
 
     # eat content
     while ts[0].t != "]":
         if ts[0].t == ":A:":
-            a, ts = assertion(ts[1:], context_name, zone_name, False)
+            a, ts = _assertion(ts[1:], context_name, zone_name, False)
             out[K_CONTENT].append(a)
         else:
             raise ValueError("expected :A: or ] at "+str(ts[0].p[0]))
-    ts = consume_symbol(ts, "]")    
+    ts = _consume_symbol(ts, "]")
 
     # and signatures, if present
-    out[K_SIGNATURES], ts = signatures(ts)
+    out[K_SIGNATURES], ts = _signatures(ts)
 
     if not is_section:
         del(out[K_SUBJECT_ZONE])
         del(out[K_CONTEXT])
 
-    if (len(out[K_SHARD_RANGE]) < 2 or 
-        (out[K_SHARD_RANGE][0] is None and 
+    if (len(out[K_SHARD_RANGE]) < 2 or
+        (out[K_SHARD_RANGE][0] is None and
             out[K_SHARD_RANGE][1] is None)):
         del(out[K_SHARD_RANGE])
 
     return out, ts
 
-def assertion(ts, context_name, zone_name, is_section):
+def _assertion(ts, context_name, zone_name, is_section):
     out = { K_SUBJECT_ZONE:    zone_name,
             K_CONTEXT:      context_name,
             K_OBJECTS:      [] }
@@ -238,16 +258,16 @@ def assertion(ts, context_name, zone_name, is_section):
     else:
         raise ValueError("Expected assertion subject name at "+str(ts[0].p[0]))
 
-    ts = consume_symbol(ts, "[")
+    ts = _consume_symbol(ts, "[")
 
     # eat object content
     while ts[0].t != "]":
-        o, ts = objekt(ts)
+        o, ts = _objekt(ts)
         out[K_OBJECTS].append(o)
-    ts = consume_symbol(ts, "]") 
+    ts = _consume_symbol(ts, "]")
 
     # and signatures, if present
-    out[K_SIGNATURES], ts = signatures(ts)
+    out[K_SIGNATURES], ts = _signatures(ts)
 
     if not is_section:
         del(out[K_SUBJECT_ZONE])
@@ -255,7 +275,7 @@ def assertion(ts, context_name, zone_name, is_section):
 
     return out, ts
 
-def objekt(ts):
+def _objekt(ts):
     if ts[0].t == ":ip4:":
         if ts[1].t != "VAL_IP4":
             raise ValueError("expected IPv4 address at "+str(ts[0].p[0]))
@@ -338,44 +358,75 @@ def objekt(ts):
     else:
         raise ValueError("expected object at at "+str(ts[0].p[0]))
 
-def signatures(ts):
+def _signatures(ts):
     out = []
 
     # check for signature
     if len(ts) and ts[0].t == "(":
         ts = ts[1:]
         while ts[0].t != ")":
-            s, ts = signature(ts)
+            s, ts = _signature(ts)
             out.append(s)
-        ts = consume_symbol(")")
+        ts = _consume_symbol(")")
 
     return out, ts
 
-def signature(ts):
-    ts = consume_symbol(":sig:", ts)
+def _signature(ts):
+    ts = _consume_symbol(":sig:", ts)
     if is_string(ts[0].t):
         alg = ALGORITHMS[ts[0].v]
     else:
         raise ValueError("expected signature algorithm at "+str(ts[0].p[0]))
 
+    if is_string(ts[1].t):
+        ks = KEYSPACES[ts[1].v]
+    else:
+        raise ValueError("expected signature keyspace at "+str(ts[0].p[0]))
+
     if ts[1].t == "VAL_8601":
-        st = ts[1].v
+        st = ts[2].v
     else:
         raise ValueError("expected start of validity at "+str(ts[0].p[0]))
 
     if ts[2].t == "VAL_8601":
-        st = ts[2].v
+        st = ts[3].v
     else:
         raise ValueError("expected end of validity at "+str(ts[0].p[0]))
 
-    ts = ts[3:]
+    ts = ts[4:]
     hexsig = ""
     while ts[0].t == "VAL_HEX":
         hexsig += ts[0].v
         ts = ts[1:]
-    
-    return [ alg, st, et, bytes.fromhex(hexkey) ], ts
 
+    return [ alg, ks, st, et, bytes.fromhex(hexkey) ], ts
+
+def compile(zstr):
+    """
+    Takes a RAINS zonefile as a string, returns a Python dict suitable for
+    serialization as a RAINS message.
+
+    """
+    ts, rest = scanner.scan(zstr)
+    return _message(ts)
+
+def decompile(z):
+    """
+    Takes a RAINS message as a Python dict, returns a pretty-printed zonefile
+    as a string
+
+    """
+    for sections in z[K_CONTENT]:
+        pass # WORK POINTER
+
+def shard_zone(z, count=None, size=None):
+    """
+    Takes a Python dict structure containing a parsed zonefile, and either a
+    shard count or a maximum shard size, and splits the zone into shards,
+    returning the modified structure.
+
+    """
+    pass
 
 test_zone_1 = """
 :Z: . example.com [
