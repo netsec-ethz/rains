67ce16fea4cd9fed1c5f6aac31561c603e1a5f5b
Brian Trammell <brian@trammell.ch>
2017-05-02 17:42:01 +0200

    random addresses work now

--- zonefiles/rzfic.py
+++ zonefiles/rzfic.py
@@ -1,8 +1,9 @@
 import argparse
-import random
 import sys
 
-from ipaddress import IPv4Address, IPv6Address
+from ipaddress import IPv4Address, IPv6Address, IPv4Network, IPv6Network
+
+from random import randrange
 from numpy.random import poisson
 
 def parse_args():
@@ -24,9 +25,9 @@ def parse_args():
     parser.add_argument("--ip6-lambda", type=float, default=1.0,
                         help="Parameter to Poisson distribution for v4 addresses per host")
     parser.add_argument("--ip4-pfxlen", type=int, default=16,
-                        help="Prefix length for IPv4 addresses in a zone (0/8/16/24)")
+                        help="Prefix length for IPv4 addresses in a zone")
     parser.add_argument("--ip6-pfxlen", type=int, default=56,
-                        help="Prefix length for IPv6 addresses in a zone (bytewise)")
+                        help="Prefix length for IPv6 addresses in a zone")
     parser.add_argument("--wordlist", type=str, default="words.txt", 
                         help="Wordlist to take entity names from")
     parser.add_argument("--out", type=str, default="-",
@@ -35,19 +36,12 @@ def parse_args():
     
     return parser.parse_args()
 
-# FIXME this doesn't work for some reason
-def random_ipv4(pfxbytes=b""):
-    addrbytes = bytearray(pfxbytes)
-    for i in range(len(pfxbytes),4):
-        addrbytes += bytearray((random.randrange(256),))
-    return str(IPv4Address(addrbytes))
+def random_ipv4(net=IPv4Network("0.0.0.0/0")):
+    return net[randrange(2 ** (32 - net.prefixlen))]
 
-# FIXME this doesn't work for some reason
-def random_ipv6(pfxbytes=b""):
-    addrbytes = bytearray(pfxbytes)
-    for i in range(len(pfxbytes),16):
-        addrbytes += bytes((random.randrange(256),))
-    return str(IPv6Address(addrbytes))
+
+def random_ipv6(net=IPv6Network("2000::/3")):
+    return net[randrange(2 ** (128 - net.prefixlen))]
 
 def generate_endhost(fp, name, v4_net, v6_net, args):
     v4_count = poisson(args.ip4_lambda)
@@ -59,12 +53,12 @@ def generate_endhost(fp, name, v4_net, v6_net, args):
     for i in range(v6_count):
         fp.write("        :ip6: %s\n" % (str(random_ipv6(v6_net))))
     for i in range(v4_count):
-        fp.write("        :ip4: 127.0.0.1\n" % (str(random_ipv4(v4_net))))
+        fp.write("        :ip4: %s\n" % (str(random_ipv4(v4_net))))
     fp.write("    ]\n")
 
 def generate_delegation(fp, name, args):
     fp.write("    :A: %s [\n" % (name))
-    fp.write("        ::deleg:: ed22519 keymaterial-goes-here\n")
+    fp.write("        :deleg: ed22519 keymaterial-goes-here\n")
     fp.write("    ]\n")
 
 def generate_zone(fp, words, args):
@@ -73,24 +67,22 @@ def generate_zone(fp, words, args):
 
     names_used = set()
 
-    v4_net = random_ipv4().packed[:4-args.ip4_prefix/8]
-    print("v4 net is "+repr(v4_net))
-    v6_net = random_ipv6().packed[:16-args.ip6_prefix/8]
-    print("v6 net is "+repr(v6_net))
+    v4_net = IPv4Network(str(random_ipv4())+"/"+str(args.ip4_pfxlen), strict=False)
+    v6_net = IPv6Network(str(random_ipv6())+"/"+str(args.ip6_pfxlen), strict=False)
 
     for i in range(args.name_count):
-        typedie = random.randrange(args.endhost_weight + 
-                                   args.delegation_weight)
+        typedie = randrange(args.endhost_weight + 
+                            args.delegation_weight)
         
         while True:
-            worddie = random.randrange(len(words))
+            worddie = randrange(len(words))
             if words[worddie] not in names_used:
                 name = words[worddie]
                 names_used.add(name)
                 break
         
         if typedie < args.endhost_weight:
-            generate_endhost(fp, name, args)
+            generate_endhost(fp, name, v4_net, v6_net, args)
         else:
             generate_delegation(fp, name, args)
 
