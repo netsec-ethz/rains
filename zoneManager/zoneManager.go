f28cf5f3f40e842f267b4bd68edcbef7f1ad64ed
Christian Fehlmann <fehlmach@student.ethz.ch>
2018-09-25 11:33:01 +0200

    Moved design documents to docs folder. Renamed example to examples

--- zoneManager/zoneManager.go
+++ zoneManager/zoneManager.go
@@ -1,11 +1,6 @@
 package main
 
-import (
-	"github.com/netsec-ethz/rains/zonepub"
-)
-
 func main() {
 	//TODO CFE generate zonefile and do sharding according to some configuration
 	//TODO CFE handle nextkey-requests
-	zonepub.Init("test/rainspub.conf")
 }
