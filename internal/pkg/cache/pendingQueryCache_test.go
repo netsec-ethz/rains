f3de2f611e09dd12ac111b560cf60c7c549a6190
Christian Fehlmann <fehlmach@student.ethz.ch>
2019-01-03 14:13:29 +0100

    Unit tests pendingKeyCache

--- internal/pkg/cache/pendingQueryCache_test.go
+++ internal/pkg/cache/pendingQueryCache_test.go
@@ -29,10 +29,10 @@ func TestPendingQueryCache(t *testing.T) {
 			t.Error("mss[0] was not added to the cache")
 		}
 		if ok := c.Add(mss[1], mss[1].Token, time.Now().Add(time.Hour).Unix()); ok || c.Len() != 2 {
-			t.Error("mss[0] was not added to the cache")
+			t.Error("mss[1] was not added to the cache")
 		}
 		if ok := c.Add(mss[2], mss[2].Token, time.Now().Add(time.Hour).Unix()); !ok || c.Len() != 3 {
-			t.Error("mss[0] was not added to the cache")
+			t.Error("mss[2] was not added to the cache")
 		}
 		//Test c.GetAndRemove()
 		if v := c.GetAndRemove(mss[1].Token); len(v) != 0 || c.Len() != 3 {
@@ -44,7 +44,7 @@ func TestPendingQueryCache(t *testing.T) {
 		}
 		if v := c.GetAndRemove(mss[0].Token); len(v) != 2 || !reflect.DeepEqual(v[0], mss[0]) ||
 			!reflect.DeepEqual(v[1], mss[1]) || c.Len() != 0 {
-			t.Error("mss[2] should be returned for this token")
+			t.Error("mss[0] and mss[1] should be returned for this token")
 		}
 		//Test c.RemoveExpiredValues()
 		c.Add(mss[0], mss[0].Token, time.Now().Add(time.Hour).Unix())
