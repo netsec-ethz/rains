37e0a9127af55659fb6522ce8327b64ac6906bab
Rayhaan Jaufeerally <rayhaan@rayhaan.ch>
2019-01-14 14:25:09 +0100

    Update SCION support in rainsd.
    Addressed comments from code review.
    
    Specifically cleaned up unnecessary structs.

--- internal/pkg/connection/connection.go
+++ internal/pkg/connection/connection.go
@@ -17,7 +17,6 @@ import (
 
 	sd "github.com/scionproto/scion/go/lib/sciond"
 	"github.com/scionproto/scion/go/lib/snet"
-	"github.com/scionproto/scion/go/lib/spath"
 )
 
 //Info contains address information about one actor of a connection of the declared type
@@ -26,6 +25,7 @@ type Info struct {
 	Addr net.Addr
 }
 
+// UnmarshalJSON implements the JSONUnmarshaler interface.
 func (c *Info) UnmarshalJSON(data []byte) error {
 	var err error
 	c.Type, c.Addr, err = UnmarshalNetAddr(data)
@@ -35,6 +35,7 @@ func (c *Info) UnmarshalJSON(data []byte) error {
 	return nil
 }
 
+// UnmarshalNetAddr is a helper function that unmarshals network addresses.
 func UnmarshalNetAddr(data []byte) (Type, net.Addr, error) {
 	m := map[string]interface{}{}
 	if err := json.Unmarshal(data, &m); err != nil {
@@ -46,7 +47,10 @@ func UnmarshalNetAddr(data []byte) (Type, net.Addr, error) {
 	case "TCP":
 		value = reflect.New(reflect.TypeOf(net.TCPAddr{})).Interface()
 		t = TCP
-		addrData, err := json.Marshal(m["Addr"])
+		if _, ok := m["TCPAddr"]; !ok {
+			return -1, nil, errors.New("TCPAddr key not found in JSON config")
+		}
+		addrData, err := json.Marshal(m["TCPAddr"])
 		if err != nil {
 			return -1, nil, err
 		}
@@ -63,7 +67,6 @@ func UnmarshalNetAddr(data []byte) (Type, net.Addr, error) {
 		if err = json.Unmarshal(addrData, &value); err != nil {
 			return -1, nil, err
 		}
-
 	case "SCION":
 		if _, ok := m["Local"]; !ok {
 			return -1, nil, errors.New("local address is required for SCION")
@@ -76,26 +79,14 @@ func UnmarshalNetAddr(data []byte) (Type, net.Addr, error) {
 		if err != nil {
 			return -1, nil, fmt.Errorf("failed to parse local addr: %v", err)
 		}
-		a := &SCIONAddr{
-			Local: scionLocal,
+		if scionLocal == nil {
+			return -1, nil, fmt.Errorf("returned SCION address was nil")
 		}
-		if _, ok := m["Remote"]; ok {
-			remote, ok := m["Remote"].(string)
-			if !ok {
-				return -1, nil, errors.New("remote address must be a string")
-			}
-			scionRemote, err := snet.AddrFromString(remote)
-			if err != nil {
-				return -1, nil, fmt.Errorf("failed to parse remote addr: %v", err)
-			}
-			a.Remote = scionRemote
-		}
-		value = a
+		value = scionLocal
 		t = SCION
 	default:
 		return -1, nil, errors.New("Unknown Addr type")
 	}
-
 	return t, value.(net.Addr), nil
 }
 
@@ -180,57 +171,13 @@ func (c *Channel) SetWriteDeadline(t time.Time) error {
 	return nil
 }
 
-// SCIONAddr is a wrapper around a SCION source and destination address.
-// This is needed because snet needs a local address to initiate the connection
-// from, and we need to check the local IA w.r.t. the remote IA to select the path.
-// Underlying contains the actual SCION connection. It is here instead of in the
-// connection cache because the connection cache semantics are for connection oriented
-// transports whilst for SCION we use UDP.
-type SCIONAddr struct {
-	Local      *snet.Addr
-	Remote     *snet.Addr
-	Underlying net.Conn
-}
-
-// Network implements net.Addr.
-// Remote only because the connection cache uses this to lookup the connection.
-func (sa *SCIONAddr) Network() string {
-	return sa.Remote.Network()
-}
-
-// String implements net.Addr.
-// Remote only because the connection cache uses this to lookup the connection.
-func (sa *SCIONAddr) String() string {
-	return sa.Remote.String()
-}
-
 //CreateConnection returns a newly created connection with connInfo or an error
 func CreateConnection(addr net.Addr) (conn net.Conn, err error) {
 	switch addr.(type) {
 	case *net.TCPAddr:
 		return tls.Dial(addr.Network(), addr.String(), &tls.Config{InsecureSkipVerify: true})
-	case *SCIONAddr:
-		// First check if the remote IA is not in the local IA.
-		// This means that we need to choose the path.
-		sa := addr.(*SCIONAddr)
-		la := sa.Local
-		ra := sa.Remote
-		if !la.IA.Eq(ra.IA) {
-			pathEntry := choosePathSCION(context.Background(), la, ra)
-			if pathEntry == nil {
-				return nil, fmt.Errorf("Failed to find path from %s to %s", la.IA, ra.IA)
-			}
-			ra.Path = spath.New(pathEntry.Path.FwdPath)
-			if err := ra.Path.InitOffsets(); err != nil {
-				return nil, fmt.Errorf("failed to init offsets to remote addr: %v", err)
-			}
-			ra.NextHop, _ = pathEntry.HostInfo.Overlay()
-		}
-		c, err := snet.DialSCION("", la, ra)
-		if err != nil {
-			return nil, fmt.Errorf("failed to DialSCION: %v", err)
-		}
-		return c, nil
+	case *snet.Addr:
+		return nil, errors.New("creating SCION connections is unsupported")
 	default:
 		return nil, errors.New("unsupported Network address type")
 	}
