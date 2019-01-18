760c59bebe002117f49871c6b2a2e33b22695e91
Christian Fehlmann <fehlmach@student.ethz.ch>
2019-01-18 14:31:24 +0100

    updates keyManager man page and implementation

--- cmd/keyManager/keyManager.go
+++ cmd/keyManager/keyManager.go
@@ -9,28 +9,38 @@ import (
 	flag "github.com/spf13/pflag"
 )
 
-var keyPath = flag.StringP("path", "p", "", "Path where the keys are or will be stored.")
-var keyName = flag.StringP("name", "n", "", "Name determines the prefix of the key pair's file name")
-var algo = flag.StringP("algo", "a", "ed25519", "Algorithm used to generate key")
-var phase = flag.Int("phase", 0, "Key phase of the generated key")
-var description = flag.StringP("description", "d", "", "description added when a new key pair is generated")
-var pwd = flag.String("pwd", "", "password to used to encrypt a newly generated key pair")
+var keyName = flag.StringP("name", "n", "", "prefix of the file name where the key is loaded from or will be stored to.")
+var algo = flag.StringP("algo", "a", "ed25519", `defines the algorithm which is used in key generation. 
+The default is ed25519. Supported algorithms are: ed25519`)
+var phase = flag.Int("phase", 0, "defines the key phase for which a key is generated. The default is 0")
+var description = flag.StringP("description", "d", "", `allows to store an arbitrary 
+string value with the key. It can e.g. be used to store the information in which zone and context 
+the key pair is used. The default is the empty string.`)
+var pwd = flag.String("pwd", "", "states the password to encrypt or decrypt a private key. The default is the empty string.")
 
 func main() {
 	flag.Parse()
-	if len(flag.Args()) == 0 {
-		log.Fatal("Please provide a command")
-	}
-	if len(flag.Args()) > 1 {
+	path := ""
+	cmd := ""
+	switch flag.NArg() {
+	case 0:
+		log.Fatal("Please provide a command and a path")
+	case 1:
+		cmd = flag.Arg(0)
+	case 2:
+		cmd = flag.Arg(0)
+		path = flag.Arg(1)
+	default:
 		log.Fatal("Too many arguments")
 	}
-	switch flag.Arg(0) {
+
+	switch cmd {
 	case "load", "l":
-		fmt.Println(keyManager.LoadPublicKeys(*keyPath))
+		fmt.Println(keyManager.LoadPublicKeys(path))
 	case "generate", "gen", "g":
-		keyManager.GenerateKey(*keyPath, *keyName, *description, *algo, *pwd, *phase)
+		keyManager.GenerateKey(path, *keyName, *description, *algo, *pwd, *phase)
 	case "decrypt", "d":
-		block := keyManager.DecryptKey(*keyPath, *keyName, *pwd)
+		block := keyManager.DecryptKey(path, *keyName, *pwd)
 		if block != nil {
 			log.Fatal("Was not able to decrypt private key")
 		}
