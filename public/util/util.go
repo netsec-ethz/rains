53c2f1a30bced0c152cbab0a3ed0aeb424eb2994
Claude Hähni <chaehni@student.ethz.ch>
2019-03-06 16:15:04 +0100

    improvements
    better address parsing, pass all parameters as arguments

--- public/util/util.go
+++ public/util/util.go
@@ -16,15 +16,13 @@ import (
 	"github.com/scionproto/scion/go/lib/snet"
 )
 
-var (
-	addrRegexp = regexp.MustCompile(`(?P<ia>\d+-[\d:A-Fa-f]+),\[(?P<host>[^\]]+)\]`)
-	context    = "."
-	timeout    = time.Second
-)
+var scionIP4Addr = regexp.MustCompile(`:scionip4:(?P<addr>\d+-[\d:A-Fa-f]+,\[[^\]]+\])`)
 
-// QueryName queries the RAINS server at 'addr' for 'name' and returns the raw Assertions corresponding to 'types'
-// 'addr' must either be a *snet.Addr or *net.TCPAddr
-func QueryName(name, types, addr string, port uint16) (*message.Message, error) {
+// QueryName queries the RAINS server at addr for name and returns the raw reply for the given type
+// addr must either be a *snet.Addr or *net.TCPAddr
+func QueryName(name, context string, types []string, opts []int, timeout time.Duration,
+	addr string, port uint16) (*message.Message, error) {
+
 	var serverAddr net.Addr
 	serverAddr, err := snet.AddrFromString(fmt.Sprintf("%s:%d", addr, port))
 	if err != nil {
@@ -35,16 +33,14 @@ func QueryName(name, types, addr string, port uint16) (*message.Message, error) 
 		}
 	}
 
-	var qTypes []object.Type
-	var opts []query.Option // TODO (chaehni): take options as argument
 	token := token.New()
-
-	qTypes, err = object.ParseTypes(types)
+	qOpts := parseQueryOption(opts)
+	qTypes := parseTypes(types)
 	if err != nil {
 		return nil, err
 	}
 
-	msg := util.NewQueryMessage(name, context, time.Now().Add(timeout).Unix(), qTypes, opts, token)
+	msg := util.NewQueryMessage(name, context, time.Now().Add(timeout).Unix(), qTypes, qOpts, token)
 	reply, err := util.SendQuery(msg, serverAddr, time.Second)
 	if err != nil {
 		return nil, err
@@ -53,16 +49,67 @@ func QueryName(name, types, addr string, port uint16) (*message.Message, error) 
 	return &reply, nil
 }
 
-// FormatSections formats []section.Section according to the zone file format
+// FormatSections formats sections according to the zone file format
 func FormatSections(sections []section.Section) string {
 	return zonefile.IO{}.Encode(sections)
 }
 
 // ParseSCIONAddr returns the SCION address contained in an Assertion
-func ParseSCIONAddr(assertion *message.Message) (string, error) {
-	parsed := addrRegexp.FindString(FormatSections(assertion.Content))
-	if parsed == "" {
-		return "", fmt.Errorf("Assertion does not contain SCION address")
+func ParseSCIONAddr(msg *message.Message) (string, error) {
+	if len(msg.Content) == 0 {
+		return "", fmt.Errorf("message is not an assertion")
 	}
-	return parsed, nil
+	switch msg.Content[0].(type) {
+	case *section.Assertion:
+		parsed := scionIP4Addr.FindStringSubmatch(FormatSections(msg.Content))
+		if len(parsed) == 0 {
+			return "", fmt.Errorf("assertion does not contain a SCION address")
+		}
+		return parsed[1], nil
+	default:
+		return "", fmt.Errorf("message is not an assertion")
+	}
+}
+
+// TODO (chaehni) do we need this validity check?
+// query.Option is just an int, we could pass the opts slice directly
+// Or make RAINS library expose the Options ( same for query Types)
+func parseQueryOption(opts []int) []query.Option {
+	var qOpts []query.Option
+	for _, opt := range opts {
+		switch opt {
+		case 1:
+			qOpts = append(qOpts, query.QOMinE2ELatency)
+		case 2:
+			qOpts = append(qOpts, query.QOMinLastHopAnswerSize)
+		case 3:
+			qOpts = append(qOpts, query.QOMinInfoLeakage)
+		case 4:
+			qOpts = append(qOpts, query.QOCachedAnswersOnly)
+		case 5:
+			qOpts = append(qOpts, query.QOExpiredAssertionsOk)
+		case 6:
+			qOpts = append(qOpts, query.QOTokenTracing)
+		case 7:
+			qOpts = append(qOpts, query.QONoVerificationDelegation)
+		case 8:
+			qOpts = append(qOpts, query.QONoProactiveCaching)
+		case 9:
+			qOpts = append(qOpts, query.QOMaxFreshness)
+		default:
+			continue
+		}
+	}
+	return qOpts
+}
+
+func parseTypes(types []string) []object.Type {
+	var qTypes []object.Type
+	for _, t := range types {
+		parsed, err := object.ParseTypes(t)
+		if err == nil {
+			qTypes = append(qTypes, parsed...)
+		}
+	}
+	return qTypes
 }
