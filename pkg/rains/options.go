cf4932f74bd448e550fa2025a99845f4a1a46a28
Claude Hähni <chaehni@student.ethz.ch>
2019-03-14 11:11:13 +0100

    removed parse/CLIString functions

--- pkg/rains/options.go
+++ pkg/rains/options.go
@@ -1,8 +1,6 @@
 package rains
 
 import (
-	"fmt"
-
 	"github.com/netsec-ethz/rains/internal/pkg/query"
 )
 
@@ -21,59 +19,6 @@ const (
 	QONoProactiveCaching
 	QOMaxFreshness
 )
-
-// ParseOptions returns the query option(s) specified in opt
-func ParseOptions(opt string) ([]Option, error) {
-	switch opt {
-	case "minEE":
-		return []Option{QOMinE2ELatency}, nil
-	case "minAS":
-		return []Option{QOMinLastHopAnswerSize}, nil
-	case "minIL":
-		return []Option{QOMinInfoLeakage}, nil
-	case "noIL":
-		return []Option{QOCachedAnswersOnly}, nil
-	case "exp":
-		return []Option{QOExpiredAssertionsOk}, nil
-	case "tracing":
-		return []Option{QOTokenTracing}, nil
-	case "noVD":
-		return []Option{QONoVerificationDelegation}, nil
-	case "noCaching":
-		return []Option{QONoProactiveCaching}, nil
-	case "maxAF":
-		return []Option{QOMaxFreshness}, nil
-	case "all":
-		return AllOptions(), nil
-	}
-	return []Option{Option(-1)}, fmt.Errorf("%s is not a query option", opt)
-}
-
-// CLIString returns the CLI type string corresponding to the query option specified in opt
-func (opt Option) CLIString() string {
-	switch opt {
-	case QOMinE2ELatency:
-		return "minEE"
-	case QOMinLastHopAnswerSize:
-		return "minAS"
-	case QOMinInfoLeakage:
-		return "minIL"
-	case QOCachedAnswersOnly:
-		return "noIL"
-	case QOExpiredAssertionsOk:
-		return "exp"
-	case QOTokenTracing:
-		return "tracing"
-	case QONoVerificationDelegation:
-		return "noVD"
-	case QONoProactiveCaching:
-		return "noCaching"
-	case QOMaxFreshness:
-		return "maxAF"
-	default:
-		return opt.String()
-	}
-}
 
 //AllOptions returns all query options
 func AllOptions() []Option {
--- pkg/rains/types.go
+++ pkg/rains/types.go
@@ -1,8 +1,6 @@
 package rains
 
 import (
-	"fmt"
-
 	"github.com/netsec-ethz/rains/internal/pkg/object"
 )
 
@@ -27,83 +25,6 @@ const (
 	OTScionAddr6
 	OTScionAddr4
 )
-
-// ParseTypes returns the object type(s) specified in qType
-func ParseTypes(t string) ([]Type, error) {
-	switch t {
-	case "name":
-		return []Type{OTName}, nil
-	case "ip6":
-		return []Type{OTIP6Addr}, nil
-	case "ip4":
-		return []Type{OTIP4Addr}, nil
-	case "scionip6":
-		return []Type{OTScionAddr6}, nil
-	case "scionip4":
-		return []Type{OTScionAddr4}, nil
-	case "redir":
-		return []Type{OTRedirection}, nil
-	case "deleg":
-		return []Type{OTDelegation}, nil
-	case "nameset":
-		return []Type{OTNameset}, nil
-	case "cert":
-		return []Type{OTCertInfo}, nil
-	case "srv":
-		return []Type{OTServiceInfo}, nil
-	case "regr":
-		return []Type{OTRegistrar}, nil
-	case "regt":
-		return []Type{OTRegistrant}, nil
-	case "infra":
-		return []Type{OTInfraKey}, nil
-	case "extra":
-		return []Type{OTExtraKey}, nil
-	case "next":
-		return []Type{OTNextKey}, nil
-	case "any":
-		return AllTypes(), nil
-	}
-	return []Type{Type(-1)}, fmt.Errorf("%s is not a object type", t)
-}
-
-// CLIString returns the CLI type string corresponding to the object type specified in t
-func (t Type) CLIString() string {
-	switch t {
-	case OTName:
-		return "name"
-	case OTIP6Addr:
-		return "ip6"
-	case OTIP4Addr:
-		return "ip4"
-	case OTScionAddr6:
-		return "scionip6"
-	case OTScionAddr4:
-		return "scionip4"
-	case OTRedirection:
-		return "redir"
-	case OTDelegation:
-		return "deleg"
-	case OTNameset:
-		return "nameset"
-	case OTCertInfo:
-		return "cert"
-	case OTServiceInfo:
-		return "srv"
-	case OTRegistrar:
-		return "regr"
-	case OTRegistrant:
-		return "regt"
-	case OTInfraKey:
-		return "infra"
-	case OTExtraKey:
-		return "extra"
-	case OTNextKey:
-		return "next"
-	default:
-		return t.String()
-	}
-}
 
 //AllTypes returns all object types
 func AllTypes() []Type {
